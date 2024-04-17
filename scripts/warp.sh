235a1d612a20bfa7ff66ab874c7bfff3c8c8b19c
tianli <34931204+zengtianli@users.noreply.github.com>
2024-04-18 07:34:37 +0800

    before app launch

---
+++ scripts/warp.sh
@@ -0,0 +1,2 @@
+osascript -e 'tell application "Warp" to activate' -e 'tell application "System Events" to keystroke "n" using {command down}'
+source ~/.config/yabai/toggle-display-center-floating-tiling3.sh
