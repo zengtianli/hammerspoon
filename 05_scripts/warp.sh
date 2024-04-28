44548325ba565706f4cd10f9747f7cd7e6a4cc1c
tianli <34931204+zengtianli@users.noreply.github.com>
2024-04-28 16:08:55 +0800

    fix float

--- 05_scripts/warp.sh
+++ 05_scripts/warp.sh
@@ -1,2 +1,15 @@
+#!/usr/bin/env bash
+current_space_index=$(yabai -m query --spaces --space mouse | jq -r .index)
+# Activate Warp and create a new window
 osascript -e 'tell application "Warp" to activate' -e 'tell application "System Events" to keystroke "n" using {command down}'
-source ~/.config/yabai/toggle-display-center-floating-tiling3.sh
+# Wait for a short time to ensure the new window is created
+sleep 0.1
+# Get the current space index
+# Get the Warp window ID
+warp_window_id=$(yabai -m query --windows --window | jq -r '.id')
+# Move the Warp window to the current space
+yabai -m window --space $current_space_index $warp_window_id
+# Focus the current space
+yabai -m space --focus $current_space_index
+# Apply the toggle-display-center-floating-tiling script to the Warp window
+source ~/.config/yabai/toggle-display-center-floating-tiling.sh
